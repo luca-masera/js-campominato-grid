9604ca60ee809fe0d9de36f3b8127ea8fcb07ba7
luca-masera <lucamasera1995@gmail.com>
2023-10-18 16:02:16 +0200

    creazione elemento div e aggiunta class css

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,15 +8,21 @@ btn.addEventListener('click',function(){
 
  // Vado a specificare il numero di quadratini che devono esserci in tutta la griglia
  const celle = 100;
- 
- //Vado a creare un ciclo per formare numeri progressivi da 1 a 100
+
+ //Creo un ciclo per formare numeri progressivi da 1 a 100
  for(let i = 0; i < celle; i++)
-
-
-
-
+ console.log(i)
 
 
 
 
 })
+
+function quadratino(){
+    // Creo una const per poi successivamente assegnargli classe css 
+    const quadratino = document.createElement ('div');
+    
+    //Aggiungo alla const la classe css
+    quadratino.classList.add('cella-gioco');
+
+}
