cf7d65c1b580bfed8527919e90684b05979aef89
luca-masera <lucamasera1995@gmail.com>
2023-10-18 16:19:39 +0200

    richiamo funzione dentro il btn

--- Javascript/script.js
+++ Javascript/script.js
@@ -12,17 +12,25 @@ btn.addEventListener('click',function(){
  //Creo un ciclo per formare numeri progressivi da 1 a 100
  for(let i = 0; i < celle; i++)
  console.log(i)
+// genero quadratino richiamando la funzione
+let quadratino = creazioneQuadratino(i);
+console.log(quadratino)
+//appendo il quadratino alla mia griglia
+grigliaGioco.append(quadratino);
 
 
 
 
 })
 
-function quadratino(){
+function creazioneQuadratino(campo){
     // Creo una const per poi successivamente assegnargli classe css 
     const quadratino = document.createElement ('div');
-    
+
     //Aggiungo alla const la classe css
-    quadratino.classList.add('cella-gioco');
+    quadratino.classList.add('cella-gioco');    
+    quadratino.innerHTML = campo +1;
+    
+    return quadratino;
 
 }
