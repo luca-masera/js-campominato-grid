771b1dbdb6a34047a02a336aea397a43bc2f4d54
luca-masera <lucamasera1995@gmail.com>
2023-10-18 15:53:12 +0200

    creazioni const e for per numeri da 1 a 100

---
+++ Javascript/script.js
@@ -0,0 +1,22 @@
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click',function(){
+  // Vado a prendere la const della griglia
+
+  const grigliaGioco = document.getElementById('campo-gioco');
+
+ // Vado a specificare il numero di quadratini che devono esserci in tutta la griglia
+ const celle = 100;
+ 
+ //Vado a creare un ciclo per formare numeri progressivi da 1 a 100
+ for(let i = 0; i < celle; i++)
+
+
+
+
+
+
+
+
+})
