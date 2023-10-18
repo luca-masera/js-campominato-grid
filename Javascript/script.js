280f745eb5f596d4a05b52d02cbcc130e976b101
luca-masera <lucamasera1995@gmail.com>
2023-10-18 16:24:08 +0200

    stefano

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,21 +5,24 @@ btn.addEventListener('click',function(){
   // Vado a prendere la const della griglia
 
   const grigliaGioco = document.getElementById('campo-gioco');
+ 
 
  // Vado a specificare il numero di quadratini che devono esserci in tutta la griglia
  const celle = 100;
 
  //Creo un ciclo per formare numeri progressivi da 1 a 100
- for(let i = 0; i < celle; i++)
- console.log(i)
-// genero quadratino richiamando la funzione
-let quadratino = creazioneQuadratino(i);
-console.log(quadratino)
-//appendo il quadratino alla mia griglia
-grigliaGioco.append(quadratino);
+ 
+ for(let i = 0; i < celle; i++){
+
+    console.log(i)
+    // genero quadratino richiamando la funzione
+    let quadratino = creazioneQuadratino(i);
+    console.log(quadratino)
+    //appendo il quadratino alla mia griglia
+    grigliaGioco.append(quadratino);
 
 
-
+}
 
 })
 
